7e784eee3017e555b9c27f671af5645fc1383dad
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-01-05 17:22:28 +0100

    AirBnB_2

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,9 @@ from os import getenv
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
-    from engine.db_storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
-    from engine.file_storage import FileStorage
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
