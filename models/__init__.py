c4e192723b42a8b2a157a11820603d70cd3be672
SaadElAmmari <giochi6@hotmail.com>
2025-01-05 10:12:58 -0800

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-initialize the models package
+This module instantiates an object of storage depending on type
+Contains the model of all objects
 """
-
 from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
