a8633cf52821e4a7d024d8641986d75531370af9
SaadElAmmari <giochi6@hotmail.com>
2025-01-05 09:51:30 -0800

    base_model.py

--- models/city.py
+++ models/city.py
@@ -1,43 +1,15 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-"""
-    contains City class to represent a city
-    contains City class to represent a city
-"""
 
-from models.base_model import BaseModel, Base
-from models.state import State
-=======
 #!/usr/bin/python
-""" City Class """
+""" holds class City"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
->>>>>>> 349642e593f860b031140b012c6861d3396e21e9
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String, ForeignKey
-from os import environ
-
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-<<<<<<< HEAD
-    """ City class :City class to represent a city
-    City class :City class to represent a city"""
-
-    if (storage_engine == "db"):
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey(State.id))
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
-    else:
-        name = ""
-        state_id = ""
-
-=======
     """Representation of city """
     if models.storage_t == "db":
         __tablename__ = 'cities'
@@ -51,4 +23,4 @@ class City(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes city"""
         super().__init__(*args, **kwargs)
->>>>>>> 349642e593f860b031140b012c6861d3396e21e9
+
