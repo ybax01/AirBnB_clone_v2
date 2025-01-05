287ca33a83b9e3a41577ba53b02c28ed7b6a4d00
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-01-05 15:36:14 +0100

    AirBnB_2

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from base_model import BaseModel, Base
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
