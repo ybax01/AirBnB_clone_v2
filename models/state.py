fdcfe459d3bdc0ccad585f44859a0ba4a875c000
SaadElAmmari <giochi6@hotmail.com>
2025-01-05 04:26:22 -0800

    state.py

--- models/state.py
+++ models/state.py
@@ -5,26 +5,29 @@ from sqlalchemy import Column, String
 from os import getenv
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+    """ State class """
+    __tablename__ = 'states'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship('City', cascade='all, delete', backref='state')
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
+        ''' File Storage relationship '''
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
+            '''
+            returns the list of City instances
+            with state_id equals to the current State.id
+            '''
+            from models import storage
+            from models.city import City
+
             city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
+            city_dict = storage.all(City)
+
+            for city in city_dict.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
