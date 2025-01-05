63ed849d0b1ea05b3c00ec2872b6daf80fb7b25a
SaadElAmmari <giochi6@hotmail.com>
2025-01-05 12:05:23 -0800

    hey

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 """
-This module instantiates an object of storage depending on type
-Contains the model of all objects
+    instantiates the storage system, and defines
+    dummy classes for further use
 """
-from os import getenv
-from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+from models.base_model import BaseModel
+from models.city import City
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.amenity import Amenity
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+from os import environ
+
+dummy_classes = {"BaseModel": BaseModel, "User": User,
+                 "Review": Review, "City": City,
+                 "State": State, "Place": Place,
+                 "Amenity": Amenity}
+
+dummy_tables = {"states": State, "cities": City,
+                "users": User, "places": Place,
+                "reviews": Review, "amenities": Amenity}
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
+if (storage_engine == "db"):
     storage = DBStorage()
+    storage.reload()
 else:
     storage = FileStorage()
-
-storage.reload()
+    storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""
+    contains City class to represent a city
+    contains City class to represent a city
+"""
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.state import State
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+    """ City class :City class to represent a city
+    City class :City class to represent a city"""
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    if (storage_engine == "db"):
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey(State.id))
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        name = ""
+        state_id = ""
