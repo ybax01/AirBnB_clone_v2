beef32d57ffea9c3cc0160a9c099cee8b78c16a0
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-02-07 19:43:19 +0100

    AirBnB_2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from flask import Flask
+"""class Flask"""
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    """
+    Returns specific text
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Returns specific text
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def display_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    and replace underscore _ symbols with a space
+    """
+    return 'C' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
