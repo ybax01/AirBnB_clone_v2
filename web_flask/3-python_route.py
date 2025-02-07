a8c4f65f89f80869dd678fa8336ce697a512fb64
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-02-07 19:50:30 +0100

    AirBnB_2

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello_hbnb():
+    """
+    Returns specific text
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Returns specific text
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    and replace underscore _ symbols with a space
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
