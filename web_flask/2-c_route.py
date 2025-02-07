dbf8bf367cbf4fac3a224e9096ebfa5ccf8255e0
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-02-07 19:44:15 +0100

    AirBnB_2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def display_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def display_text(text):
     """
     display “C ” followed by the value of the text variable
