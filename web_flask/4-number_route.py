afb38a5c01bddbbf3728f3b3bf9c884a466d867e
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-02-07 19:56:18 +0100

    AirBnB_2

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -40,5 +40,11 @@ def display_python(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
